2eac98a461e43dd6c00fe2017df22bfe867f7e38
tejutejas05 <tejasmr05@gmail.com>
2026-03-23 22:36:45 +0530

    port declaration

--- main.go
+++ main.go
@@ -1,4 +1,20 @@
 package main
 
-import "fmt"
+import(
+	"github.com/tejutejas05/Ecommerce-Website-using-GO-/controllers"
+	"github.com/tejutejas05/Ecommerce-Website-using-GO-/database"
+	"github.com/tejutejas05/Ecommerce-Website-using-GO-/middleware"
+	"github.com/tejutejas05/Ecommerce-Website-using-GO-/routes"
+	//"github.com/tejutejas05/Ecommerce-Website-using-GO-/models"
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	port := os.Getevn("PORT")
+	if port == ""{
+		port = "8000"
+	}
+
+	app := controllers.NewApplication(database.ProductData(database.Client, "Products"), database.UserData(database.Client, "Users"))
+}
 
