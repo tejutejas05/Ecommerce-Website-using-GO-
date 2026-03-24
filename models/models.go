e2a3c0ccf874a7b74c686328b96150b250ab91e1
tejutejas05 <tejasmr05@gmail.com>
2026-03-24 22:21:25 +0530

    struct declared

--- models/models.go
+++ models/models.go
@@ -9,14 +9,14 @@ import(
 
 type User struct{
 
-	ID 
-	First_Name
-	Last_Name
-	Password
-	Email
-	Phone
-	Token
-	Refresh_token
+	ID							primitive.ObjectID
+	First_Name					*string
+	Last_Name					*string
+	Password					*string
+	Email						*string
+	Phone						*string
+	Token						*string
+	Refresh_token				*string
 	Created_At
 	Updated_At
 	User_ID
@@ -27,24 +27,46 @@ type User struct{
 
 type Product struct{
 
-
+	Product_ID
+	Product_Name
+	Price
+	Rating
+	Image
 }
 
 type ProductUser struct{
 
+	Product_ID
+	Product_Name
+	Price
+	Rating
+	Image
 }
 
 type Adress struct{
-
+	Address_id
+	House
+	Street
+	City
+	Pincode
 
 }
 
 type Order struct{
+	Order_ID
+	Order_Cart
+	Ordered_At
+	Price
+	Discount
+	Payment_Method
 
 
 }
 
 type Payment struct{
 
-	
+	Digital bool
+	COD bool
+
+
 }
