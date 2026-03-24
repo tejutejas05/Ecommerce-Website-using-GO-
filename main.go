180159bc70e91f70e63aa4ad49b014f918b6bc7f
tejutejas05 <tejasmr05@gmail.com>
2026-03-24 21:46:33 +0530

    main.go updated and routing connection

--- main.go
+++ main.go
@@ -16,7 +16,17 @@ func main() {
 
 	app := controllers.NewApplication(database.ProductData(database.Client, "Products"), database.UserData(database.Client, "Users"))
 
-	port_number := 8000
-	
-}
+	router := gin.New()
+	router.Use(gin.Logger())
 
+	routes.UserRoutes(router)
+	router.Use(middleware.Authentication())
+
+	router.GET("/addtocart", app.AddToCart())
+	router.GET("removeitem", app.RemoveItem())
+	router.GET("/cartcheckout", app.BuyFromCart())
+	router.GET("/instantbuy", app.InstantBuy())
+
+	log.Fatal(router.Run(':' + port))
+
+}
